f6be2d2c644743a40b170626f640c0a2c3deaec3
Jonathan Natanael Siahaan <jonathannael12@gmail.com>
2018-09-29 08:59:41 +0700

    Add more metrics on Dashboard

--- src/variables/charts.jsx
+++ src/variables/charts.jsx
@@ -65,6 +65,57 @@ const dailySalesChart = {
   }
 };
 
+const totalPendukungSebagaiSaksiChart = {
+  data: {
+    labels: [
+      "Saksi",
+      "Bukan Saksi"
+    ],
+    series: [[542, 443]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 1000,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
 // ##############################
 // // // Email Subscriptions
 // #############################
@@ -186,5 +237,6 @@ const completedTasksChart = {
 module.exports = {
   dailySalesChart,
   emailsSubscriptionChart,
-  completedTasksChart
+  completedTasksChart,
+  totalPendukungSebagaiSaksiChart
 };
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -35,7 +35,8 @@ import { bugs, website, server } from "variables/general";
 import {
   dailySalesChart,
   emailsSubscriptionChart,
-  completedTasksChart
+  completedTasksChart,
+  totalPendukungSebagaiSaksiChart
 } from "variables/charts";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
@@ -45,7 +46,12 @@ class Dashboard extends React.Component {
     super(props);
 
     this.state = {
-      followers: 0
+      followers: 0,
+      witness: 0,
+      notConfirmedFollowers: 0,
+      pendukungPerDaerah: [],
+      pendukungPerProvinsi: [],
+      pendukungPerKabupaten: []
     };
 
     this.getAllPendukung();
@@ -72,13 +78,82 @@ class Dashboard extends React.Component {
     .then(result => {
       var data = result.data;
       var counter = 0;
+      var witnessCounter = 0;
+      var notConfirmedFollowerCounter = 0;
+      var pendukungPerDaerah = [];
+      var indexCounterPerDaerah = 1;
+      var pendukungPerProvinsi = {};
+      var pendukungPerKabupaten = {};
+
       Object.keys(data).map((key) => {
-        var pendukungs = data[key].pendukungs;
+        var tempPendukungPerDaerah = [];
+        tempPendukungPerDaerah.push(indexCounterPerDaerah.toString());
+        var row = data[key];
+        var pendukungs = row.pendukungs;
         for(var i = 0; i < pendukungs.length; i++) {
           counter++;
+
+          if(pendukungs[i].witness == true) {
+            witnessCounter++;
+          }
+
+          if(pendukungs[i].status == false) {
+            notConfirmedFollowerCounter++;
+          }
         }
-      })
-      this.setState({ followers: counter });
+
+        if(pendukungPerProvinsi.hasOwnProperty(row.provinsi)) {
+          pendukungPerProvinsi[row.provinsi] = pendukungPerProvinsi[row.provinsi] + pendukungs.length;
+        } else {
+          pendukungPerProvinsi[row.provinsi] = pendukungs.length;
+        }
+
+        if(pendukungPerKabupaten.hasOwnProperty(row.kabupaten)) {
+          pendukungPerKabupaten[row.kabupaten] = pendukungPerKabupaten[row.kabupaten] + pendukungs.length;
+        } else {
+          pendukungPerKabupaten[row.kabupaten] = pendukungs.length;
+        }
+
+        tempPendukungPerDaerah.push(row.kabupaten);
+        tempPendukungPerDaerah.push(row.kecamatan);
+        tempPendukungPerDaerah.push(row.kelurahan);
+        tempPendukungPerDaerah.push(row.provinsi);
+        tempPendukungPerDaerah.push(pendukungs.length.toString());
+
+        pendukungPerDaerah.push(tempPendukungPerDaerah);
+        indexCounterPerDaerah++;
+      });
+
+      var dataPendukungPerProvinsi = [];
+      var counterPendukungPerProvinsi = 1;
+      Object.keys(pendukungPerProvinsi).map((key) => {
+        var tempPendukungPerProvinsi = [];
+        tempPendukungPerProvinsi.push(counterPendukungPerProvinsi.toString());
+        tempPendukungPerProvinsi.push(key);
+        tempPendukungPerProvinsi.push(pendukungPerProvinsi[key].toString());
+        counterPendukungPerProvinsi++;
+        dataPendukungPerProvinsi.push(tempPendukungPerProvinsi);
+      });
+
+      var dataPendukungPerKabupaten = [];
+      var counterPendukungPerKabupaten = 1;
+      Object.keys(pendukungPerKabupaten).map((key) => {
+        var tempPendukungPerKabupaten = [];
+        tempPendukungPerKabupaten.push(counterPendukungPerKabupaten.toString());
+        tempPendukungPerKabupaten.push(key);
+        tempPendukungPerKabupaten.push(pendukungPerKabupaten[key].toString());
+        counterPendukungPerKabupaten++;
+        dataPendukungPerKabupaten.push(tempPendukungPerKabupaten);
+      });
+
+      this.setState({ 
+        followers: counter, 
+        witness: witnessCounter, 
+        notConfirmedFollowers: notConfirmedFollowerCounter,
+        pendukungPerDaerah: pendukungPerDaerah,
+        pendukungPerProvinsi: dataPendukungPerProvinsi,
+        pendukungPerKabupaten: dataPendukungPerKabupaten
+      });
     })
   }
 
@@ -96,13 +171,51 @@ class Dashboard extends React.Component {
                 <br/>
                 <br/>
                 <br/>
-                <p className={classes.cardCategory}>Followers</p>
+                <p className={classes.cardCategory}>Jumlah Pendukung</p>
                 <h3 className={classes.cardTitle}>+{this.state.followers}</h3>
               </CardHeader>
               <CardFooter stats>
                 <div className={classes.stats}>
                   <Update />
                   Just Updated
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="warning" stats icon>
+                <CardIcon color="warning">
+                  <Icon>content_copy</Icon>
+                </CardIcon>
+                <p className={classes.cardCategory}>Total Pendukung Sebagai Saksi</p>
+                <h3 className={classes.cardTitle}>
+                  {this.state.witness}
+                </h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <Update />
+                  Just Updated
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="danger" stats icon>
+                <CardIcon color="danger">
+                  <Icon>info_outline</Icon>
+                </CardIcon>
+                <p className={classes.cardCategory}>
+                  Total Pendukung Yang Belum Di Confirm
+                </p>
+                <h3 className={classes.cardTitle}>{this.state.notConfirmedFollowers}</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <LocalOffer />
+                  Tracked from Daftar Pendukung
                 </div>
               </CardFooter>
             </Card>
@@ -132,6 +245,59 @@ class Dashboard extends React.Component {
                   <AccessTime /> updated 4 minutes ago
                 </div>
               </CardFooter>
+            </Card>
+          </GridItem>
+         
+          <GridItem xs={12} sm={12} md={6}>
+            <Card>
+              <CardHeader color="warning">
+                <h4 className={classes.cardTitleWhite}>Jumlah Pendukung Untuk Setiap Daerah</h4>
+                <p className={classes.cardCategoryWhite}>
+                  
+                </p>
+              </CardHeader>
+              <CardBody>
+                <Table
+                  tableHeaderColor="warning"
+                  tableHead={["ID", "Kabupaten", "Kecamatan", "Kelurahan", "Provinsi", "Jumlah Pendukung"]}
+                  tableData={this.state.pendukungPerDaerah}
+                />
+              </CardBody>
+            </Card>
+          </GridItem>
+
+          <GridItem xs={12} sm={12} md={6}>
+            <Card>
+              <CardHeader color="warning">
+                <h4 className={classes.cardTitleWhite}>Jumlah Pendukung Untuk Setiap Provinsi</h4>
+                <p className={classes.cardCategoryWhite}>
+                  
+                </p>
+              </CardHeader>
+              <CardBody>
+                <Table
+                  tableHeaderColor="warning"
+                  tableHead={["ID", "Provinsi", "Jumlah Pendukung"]}
+                  tableData={this.state.pendukungPerProvinsi}
+                />
+              </CardBody>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card>
+              <CardHeader color="warning">
+                <h4 className={classes.cardTitleWhite}>Jumlah Pendukung Untuk Setiap Kabupaten</h4>
+                <p className={classes.cardCategoryWhite}>
+                  
+                </p>
+              </CardHeader>
+              <CardBody>
+                <Table
+                  tableHeaderColor="warning"
+                  tableHead={["ID", "Kabupaten", "Jumlah Pendukung"]}
+                  tableData={this.state.pendukungPerKabupaten}
+                />
+              </CardBody>
             </Card>
           </GridItem>
         </GridContainer>
