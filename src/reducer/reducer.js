d341c2c596eb2392a7634816af41b975ae72709a
Jonathan Natanael Siahaan <jonathannael12@gmail.com>
2018-11-26 23:09:45 +0800

    Refactor reducer name

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,13 +1,13 @@
 import { combineReducers } from "redux";
 
-import selected_pendukung_reducer from './selected_pendukung_reducer'
-import users_reducer from './users_reducer'
+import SelectedPendukungReducer from './SelectedPendukungReducer'
+import UsersReducer from './UsersReducer'
 
 class Reducer {
   static all() {
     return combineReducers({
-        selected_pendukung: selected_pendukung_reducer.reduce,
-        users: users_reducer.reduce,
+        selected_pendukung: SelectedPendukungReducer.reduce,
+        users: UsersReducer.reduce,
     });
   }
 }
