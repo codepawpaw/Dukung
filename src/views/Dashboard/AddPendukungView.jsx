bad4845f8f76456abeb2dcd29cb616ccd63fb6e1
Jonathan Natanael Siahaan <jonathannael12@gmail.com>
2018-12-17 22:04:48 +0700

    Compress image part1

--- src/views/Dashboard/AddPendukungView.jsx
+++ src/views/Dashboard/AddPendukungView.jsx
@@ -68,16 +68,77 @@ class AddPendukungView extends React.Component {
             wt = "1";
         }
 
-        this.addDukungan({
-            uploadfile: this.state.file, 
-            nik: nik,
-            phone: phone,
-            witness: wt,
-            firstname: firstname,
-            uploadfile: this.uploadfile,
-            address: address,
-            idcalon: this.props.calon.id
+        this.compressImage(this.uploadfile, (compressedImage) => {
+            this.addDukungan({
+                nik: nik,
+                phone: phone,
+                witness: wt,
+                firstname: firstname,
+                uploadfile: compressedImage,
+                address: address,
+                idcalon: this.props.calon.id
+            });
         });
+    }
+
+    dataURLToBlob(dataURL) {
+        var BASE64_MARKER = ';base64,';
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            var parts = dataURL.split(',');
+            var contentType = parts[0].split(':')[1];
+            var raw = parts[1];
+    
+            return new Blob([raw], {type: contentType});
+        }
+    
+        var parts = dataURL.split(BASE64_MARKER);
+        var contentType = parts[0].split(':')[1];
+        var raw = window.atob(parts[1]);
+        var rawLength = raw.length;
+    
+        var uInt8Array = new Uint8Array(rawLength);
+    
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+    
+        return new Blob([uInt8Array], {type: contentType});
+    }
+
+    compressImage(image, callback) {
+        var reader = new FileReader();
+        reader.readAsDataURL(image);
+
+        reader.onload = (readerEvent) => {
+            var image = new Image();
+
+            image.onload = (imageEvent) => {
+                var canvas = document.createElement('canvas'),
+                    max_size = 144,// TODO : pull max size from a site config
+                    width = image.width,
+                    height = image.height;
+
+                if (width > height) {
+                    if (width > max_size) {
+                        height *= max_size / width;
+                        width = max_size;
+                    }
+                } else {
+                    if (height > max_size) {
+                        width *= max_size / height;
+                        height = max_size;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+                var dataUrl = canvas.toDataURL('image/png');
+                var resizedImage = this.dataURLToBlob(dataUrl);
+                callback(resizedImage);
+            }
+
+            image.src = readerEvent.target.result;
+        }
     }
 
     addDukungan(data) {
